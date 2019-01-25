38177ff67a325da122274ca0b0579da82d3ced40
Nur Islam <nurislam0333@gmail.com>
2019-01-25 18:02:26 +0600

    data

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 
 // Main function
 func main() {
-	// Hardcoded data - @todo: add database
+	// data 
 	data.Books = append(data.Books, model.Book{ID: "1", Isbn: "438227", Title: "Book One", Author: &model.Author{Firstname: "John", Lastname: "Doe"}})
 	data.Books = append(data.Books, model.Book{ID: "2", Isbn: "454555", Title: "Book Two", Author: &model.Author{Firstname: "Steve", Lastname: "Smith"}})
 
